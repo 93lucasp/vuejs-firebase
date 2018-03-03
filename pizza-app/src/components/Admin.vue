1e67cad5c0066f4559b280527eae19f3c1eb7d71
93lucasp <spezzanoluca@gmail.com>
2018-03-03 12:21:30 +0100

    doing test

--- pizza-app/src/components/Admin.vue
+++ pizza-app/src/components/Admin.vue
@@ -17,6 +17,7 @@
                     <tr >
                         <td>{{item.name}}</td>
                         <td> <button class="btn btn-sm btn-danger" @click="removeMenuItem(item['.key'])">x</button> </td>
+                        <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" @click="toModal(item)">Edit</button></td>
                     </tr>
                 </tbody>
             </table>
@@ -53,6 +54,27 @@
             <vue-login></vue-login>
         </div>
     </div>
+    
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">{{item}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
@@ -63,6 +85,11 @@ import { mapGetters } from 'vuex'
 import { dbMenuRef } from '../firebaseConfig'
 import { dbOrdersRef } from '../firebaseConfig'
 export default {
+    data() {
+      return {
+          item: {} 
+      }
+  },
   components: {
       'vue-newPizza': NewPizza,
       'vue-login': Login
@@ -87,8 +114,19 @@ export default {
       removeMenuItem(key) {
           dbMenuRef.child(key).remove()
       },
+     updateMenuItem(key) {
+           dbMenuRef.child(key).update({
+              name: 'updated only title',
+              description: 'teeeest',
+          
+          });
+        //   dbMenuRef.child(key).remove()
+      },
       removeOrderItem(key) {
           dbOrdersRef.child(key).remove()
+      },
+      toModal(item) {
+          this.item = item;
       }
   }
 }
